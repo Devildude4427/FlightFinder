33f427bccdc7c6cab1282f9207249a8603af0436
Devildude4427 <ryanchristian4427@gmail.com>
2019-04-13 23:46:48 +0100

    Initializing Repository

---
+++ UI/Program.fs
@@ -0,0 +1,19 @@
+﻿module UI.Program
+
+module Program =
+
+    open Gtk
+
+    [<EntryPoint>]
+    let main argv =
+        Application.Init()
+
+        let app = new Application("org.GtkNamespace.GtkNamespace", GLib.ApplicationFlags.None)
+        app.Register(GLib.Cancellable.Current) |> ignore;
+
+        let win = new MainWindow.MainWindow()
+        app.AddWindow(win)
+
+        win.Show()
+        Application.Run()
+        0
