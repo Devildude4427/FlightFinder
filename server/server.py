783d60cb885ed23783ec899f55ca7c913bad3942
Ryan Christian <ryanchristian4427@gmail.com>
2019-07-05 01:04:37 -0500

    Project is halted until SkyScanner API is fixed. Currently return routing is broken.

--- server/server.py
+++ server/server.py
@@ -1,6 +1,5 @@
-from server.domain.form_data import FormData
-import urllib.request
-from flask import Flask, render_template, request
+# import urllib.request
+from flask import Flask, render_template, jsonify, url_for
 
 server = Flask(__name__, static_folder="../static", template_folder="../templates")
 
@@ -14,16 +13,21 @@ def landing_page():
     return render_template("index.html")
 
 
-def create_api_request(form_data):
-    api_request = urllib.request.Request(
-        "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/US/USD/en-US/"
-        + "BRS-sky/anywhere/2019-09-01?inboundpartialdate=anytime"
-    )
-    api_request.add_header(
-        "X-RapidAPI-Host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
-    )
-    api_request.add_header(
-        "X-RapidAPI-Key", "70f8ad8a68mshf3eb22144cd2fbbp1c6840jsn4efbf8230f95"
-    )
-    resp = urllib.request.urlopen(api_request)
-    content = resp.read()
+@server.route("/destinations",  methods=["POST"])
+def destinations_data():
+    return jsonify("hello")
+
+
+# def create_api_request(form_data):
+#     api_request = urllib.request.Request(
+#         "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/US/USD/en-US/"
+#         + "BRS-sky/anywhere/2019-09-01?inboundpartialdate=anytime"
+#     )
+#     api_request.add_header(
+#         "X-RapidAPI-Host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+#     )
+#     api_request.add_header(
+#         "X-RapidAPI-Key", "70f8ad8a68mshf3eb22144cd2fbbp1c6840jsn4efbf8230f95"
+#     )
+#     resp = urllib.request.urlopen(api_request)
+#     content = resp.read()
