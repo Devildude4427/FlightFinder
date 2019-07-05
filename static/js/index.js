913bb9e1778493b0e7867df81d2d8d8d41b418a3
Ryan Christian <ryanchristian4427@gmail.com>
2019-07-04 20:57:47 -0500

    Have redone the autocomplete, which now sets the iata code necessary for the flight query

--- static/js/index.js
+++ static/js/index.js
@@ -1,98 +1,49 @@
-// window.addEventListener("load", function(){
-//     const name_input = document.getElementById('portOutbound');
-//     name_input.addEventListener("keyup", function(event){hinter(event)});
-//
-//     window.hinterXHR = new XMLHttpRequest();
-// });
-//
-// function hinter(event) {
-//     const input = event.target;
-//
-//     const huge_list = document.getElementById('huge_list');
-//
-//     const min_characters = 1;
-//
-//     if (input.value.length > min_characters ) {
-//         // abort any pending requests
-//         window.hinterXHR.abort();
-//
-//         window.hinterXHR.onreadystatechange = function() {
-//             if (this.readyState === 4 && this.status === 200) {
-//                 // We're expecting a json response so we convert it to an object
-//                 const response = JSON.parse( this.responseText );
-//
-//                 // clear any previously loaded options in the datalist
-//                 huge_list.innerHTML = "";
-//
-//                 response.forEach(function(item) {
-//                     // Create a new <option> element.
-//                     const option = document.createElement('option');
-//                     option.value = item.PlaceName;
-//
-//                     // attach the option to the datalist element
-//                     huge_list.appendChild(option);
-//                 });
-//                 input.blur();
-//             }
-//             input.focus();
-//         };
-//
-//         window.hinterXHR.open("GET", "https://www.skyscanner.net/g/autosuggest-flights/GB/en-GB/" + input.value +"?ccy=GBP", true);
-//         window.hinterXHR.send()
-//     }
-// }
-
-const input = document.getElementById('portOutbound');
+let outboundIATACode = "";
 
 new autoComplete({
-    data: {                              // Data src [Array, Function, Async] | (REQUIRED)
-        src: async () => {
-            // Fetch External Data Source
-            const source = await fetch(`https://www.skyscanner.net/g/autosuggest-flights/GB/en-GB/` + input.value + `?ccy=GBP`);
-            // Format data into JSON
-            const data = await source.json();
-            // Return Fetched data
-            let places = [];
-            data.forEach(function(location) {
-                let PlaceId = location.PlaceId;
-                if (location.PlaceId === location.CityId) {
-                    PlaceId = "Any"
+    selector: 'input[id="portOutbound"]',
+    minChars: 3,
+    delay: 250,
+    source: function (term, suggest) {
+        const apiRequest = new Request('https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/' +
+            'apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=' + term, {
+            headers: {
+                    "X-RapidAPI-Host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+                    "X-RapidAPI-Key": "70f8ad8a68mshf3eb22144cd2fbbp1c6840jsn4efbf8230f95"
+            }
+        });
+
+        fetch(apiRequest)
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
                 }
-                places.push(location.PlaceName + " (" + PlaceId + ") " + location.CountryName)
+            })
+            .then(data => {
+                let places = [];
+                const {Places} = data;
+                Places.forEach(location => {
+                    places.push(location);
+                });
+                suggest(places);
             });
-            return places;
-        },
-        key: ["title"],
-        cache: false
     },
-    sort: (a, b) => {                    // Sort rendered results ascending | (Optional)
-        if (a.match < b.match) return -1;
-        if (a.match > b.match) return 1;
-        return 0;
+    renderItem: function (item, search){
+        let {PlaceId} = item;
+        const {CityId, PlaceName, CountryName} = item;
+
+        const uncutPlaceId = PlaceId;
+        if (PlaceId === CityId) PlaceId = "Any";
+        else PlaceId = PlaceId.slice(0, -4);
+
+        return '<div class="autocomplete-suggestion" outbound-iata-code="' + uncutPlaceId + '" ' +
+               'data-val="' + PlaceName + ' (' + PlaceId + ') ' + CountryName +'">' +
+                    '<b>' + PlaceName + '</b>' +
+                    ' (' + PlaceId + ') ' + CountryName +'' +
+               '</div>'
     },
-    selector: "#portOutbound",           // Input field selector              | (Optional)
-    threshold: 2,                        // Min. Chars length to start Engine | (Optional)
-    debounce: 500,                       // Post duration for engine to start | (Optional)
-    searchEngine: "strict",              // Search Engine type/mode           | (Optional)
-    resultsList: {                       // Rendered results list object      | (Optional)
-        render: true,
-        // container: source => {
-        //     return "autoComplete_Results_List";
-        // },
-        destination: document.querySelector("#portOutbound"),
-        position: "afterend",
-        element: "ul"
-    },
-    maxResults: 5,                         // Max. number of rendered results | (Optional)
-    highlight: true,                       // Highlight matching results      | (Optional)
-    resultItem: {                          // Rendered result item            | (Optional)
-        content: (data, source) => {
-            source.innerHTML = data.match;
-        },
-        element: "li"
-    },
-    onSelection: feedback => {             // Action script onSelection event | (Optional)
-        input.value = feedback.selection.value;
+    onSelect: function (e, term, item) {
+        outboundIATACode = item.getAttribute('outbound-iata-code');
     }
 });
 
