c044d675f5b1c6b84f69f899081c009aa30e116b
Ryan Christian <ryanchristian4427@gmail.com>
2019-07-08 20:43:24 -0500

    API return data can more or less be processed, though optimization will be needed

--- server/services/data_service.py
+++ server/services/data_service.py
@@ -4,19 +4,34 @@ data_file = load(open("server/data/data.json"))
 
 
 def process_response():
-    quotes = data_file['Quotes']
-    places = data_file['Places']
-    carriers = data_file['Carriers']
+    quotes = data_file["Quotes"]
+    places = data_file["Places"]
+    carriers = data_file["Carriers"]
+
+    results = 0
 
     for quote in quotes:
-        if quote['MinPrice'] > 40.00:
+        # Price capped at 40 GBP one way, and only direct flights
+        if quote["MinPrice"] > 40.00 or not quote["Direct"]:
             continue
+        results += 1
         for place in places:
-            if place['PlaceId'] == quote['OutboundLeg']['DestinationId']:
-                quote['OutboundLeg']['DestinationId'] = place['Name']
-                print(quote['OutboundLeg']['DestinationId'])
+            if place["PlaceId"] == quote["OutboundLeg"]["DestinationId"]:
+                quote["OutboundLeg"]["DestinationId"] = place["Name"]
+                break
+        for carrier in carriers:
+            if carrier["CarrierId"] in quote["OutboundLeg"]["CarrierIds"]:
+                quote["OutboundLeg"]["CarrierIds"] = carrier["Name"]
+                break
 
-    # for x in places:
-    #     if x['PlaceId'] == 42795:
-    #         print(x['IataCode'])
-    #         print(x['Name'])
+        print(
+            "Quote Id: {}, Destinations: {}, Price: {}, Carrier: {}, Direct: {}".format(
+                quote["QuoteId"],
+                quote["OutboundLeg"]["DestinationId"],
+                quote["MinPrice"],
+                quote["OutboundLeg"]["CarrierIds"],
+                quote["Direct"]
+            )
+        )
+
+    print("The search came back with {} valid results".format(results))
